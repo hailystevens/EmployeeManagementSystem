c872251da710638ec8775e1f60465fe57a772fc1
Haily S <Hailybellestevens@gmail.com>
2024-07-18 21:14:12 -0400

    first commit

--- src/main/java/com/example/ems_backend/repositoy/EmployeeRepository.java
+++ src/main/java/com/example/ems_backend/repositoy/EmployeeRepository.java
@@ -4,4 +4,6 @@ import com.example.ems_backend.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
 }
